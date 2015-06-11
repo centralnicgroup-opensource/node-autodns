f184bd91dce22e3300354633218130dc528ac9c2
Kevin Ingersoll <kingersoll@gmail.com>
2015-06-10 18:25:45 -0700

    Set up zone defaults

--- src/autodns.js
+++ src/autodns.js
@@ -1,9 +1,52 @@
 function AutoDNS (opts) {
-	opts.url = opts.url || 'https://gateway.autodns.com'
-	opts.language = opts.language || 'en'
-
-	this.options = opts
+	this.options = {
+		url: opts.url || 'https://gateway.autodns.com',
+		language: opts.language || 'en'
+	}
+	this.defaults = {}
 }
+
+
+AutoDNS.prototype.setZoneMain = function (main) {
+	var zone = this.defaults.zone = this.defaults.zone || {}
+	zone.ns_action = zone.ns_action || 'complete'
+	zone.main = {
+		value: main.value || main,
+		ttl: main.ttl || '86400'
+	}
+}
+
+AutoDNS.prototype.setZoneSOA = function (soa) {
+	var zone = this.defaults.zone = this.defaults.zone || {}
+	zone.ns_action = zone.ns_action || 'complete'
+	zone.soa = {}
+
+	if (+soa.level) {
+		zone.soa.level = soa.level
+	}
+	else {
+		// Recommended settings from AutoDNS docs
+		zone.soa.level = '0'
+		zone.soa.ttl = soa.ttl || '86400'
+		zone.soa.refresh = soa.refresh || '43200'
+		zone.soa.retry = soa.retry || '7200'
+		zone.soa.expire = soa.expire || '1209600'
+	}
+
+	if ('ignore' in soa) zone.soa.ignore = soa.ignore
+	if ('email' in soa) zone.soa.email = soa.email
+}
+
+AutoDNS.prototype.setZoneNameservers = function (nameservers) {
+	var zone = this.defaults.zone = this.defaults.zone || {}
+	zone.nserver = nameservers.map(function (ns) {
+		return {
+			name: ns.name || ns,
+			ttl: ns.ttl || '86400'
+		}
+	})
+}
+
 
 AutoDNS.prototype.createZone = function () {
 	
