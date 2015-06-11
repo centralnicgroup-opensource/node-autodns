af4c7ec704a5abdd3516ff623b6b3dfb80a2cb42
Kevin Ingersoll <kingersoll@gmail.com>
2015-06-10 18:21:04 -0700

    Separate zone create builder/parser, wrap with auth data

--- test/xml.test.js
+++ test/xml.test.js
@@ -6,16 +6,31 @@ var xml = new XML({
 	headless: true
 })
 
+// Options that are normally contained within AutoDNS
+var options = {
+	user: 'test',
+	password: 'test'
+}
+
 describe('XML', function () {
 	context('.build', function () {
 		it('should return valid XML', function () {
-			var req = xml.build({
-				user: 'test',
-				password: 'test'
-			})
+			var req = xml.build(options)
 
 			expect(req).to.be.a('string')
 			expect(req).to.equal('<request><auth><user>test</user><password>test</password></auth></request>')
+		})
+	})
+	context('.ZoneCreate', function () {
+		context('.build', function () {
+			it('should return valid XML', function () {
+				var req = xml.ZoneCreate.build(options, {
+					records: ''
+				})
+
+				expect(req).to.be.a('string')
+				expect(req).to.equal('<request><auth><user>test</user><password>test</password></auth></request>')
+			})
 		})
 	})
 })
