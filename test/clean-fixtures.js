bcd57013f8cb35a545da00c08244e1b577e8a652
Kevin Ingersoll <kingersoll@gmail.com>
2015-06-19 15:54:54 -0700

    Attempt to detect sensitive data leaks in fixtures

--- test/clean-fixtures.js
+++ test/clean-fixtures.js
@@ -11,6 +11,7 @@ var replacements = {
 
 files.forEach(function (file) {
 	var requests = require(file)
+	var hasSensitiveData = false
 
 	requests.forEach(function (req) {
 		Object.keys(replacements).forEach(function (type) {
@@ -19,8 +20,23 @@ files.forEach(function (file) {
 
 			req.body = req.body.replace(new RegExp('(<auth>.*<' + type + '>)' + value + '(</' + type + '>.*</auth>)'), '$1' + replacements[type] + '$2')
 			req.response = req.response.replace(new RegExp('(<object><type>' + type + '</type><value>)' + value + '(</value></object>)'), '$1' + replacements[type] + '$2')
+
+			if (type === 'user' || type === 'password') {
+				var pattern = new RegExp('.{0,10}' + value + '.{0,10}', 'g')
+				var matches = JSON.stringify(req).match(pattern)
+				if (matches) {
+					matches.forEach(function (match) {
+						console.warn('Found potential data leak:', match)
+					})
+					hasSensitiveData = true
+				}
+			}
 		})
 	})
+
+	if (hasSensitiveData) {
+		throw new Error('Found potential data leak')
+	}
 
 	fs.writeFileSync(file, JSON.stringify(requests, null, 2))
 })
